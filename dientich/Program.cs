fee0fbfb73399cbfd4be905240de3ef9ae302f96
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-08 11:25:48 +0700

    Add project files.

---
+++ dientich/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace dientich
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width,height;
+
+            Console.Write("width = ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("height=");
+            height = float.Parse(Console.ReadLine());
+            float dientich = width * height;
+            Console.WriteLine("Dien tich HCN la: " + dientich);
+
+            Console.ReadKey();
+        }
+    }
+}
+
